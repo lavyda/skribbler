6ccc22683d2db3d490cb40fcc3d6eb6bd0a84925
Vladimir Kovalsky <145566563+vladimirkovalsky-visma@users.noreply.github.com>
2026-06-27 13:14:22 +0200

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js"
+import tseslint from "typescript-eslint"
+import eslintPluginAstro from "eslint-plugin-astro"
+import svelte from "eslint-plugin-svelte"
+import svelteConfig from "./svelte.config.js"
+
+export default tseslint.config(
+  { ignores: ["dist/", ".astro/"] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  ...svelte.configs.recommended,
+  {
+    files: ["**/*.svelte", "**/*.svelte.ts", "**/*.svelte.js"],
+    languageOptions: {
+      parserOptions: { parser: tseslint.parser, svelteConfig },
+    },
+  },
+  {
+    // project rule overrides go here
+    rules: {},
+  },
+)
