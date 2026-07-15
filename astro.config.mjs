8e3dbdeac4ac94b126c0b23dbcd2a55cdcfbc50b
Vladimir Kovalsky <34453456+lavyda@users.noreply.github.com>
2026-07-15 21:11:41 +0200

    locale routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()],
+  integrations: [svelte(), mdx()],
   i18n: {
     locales: ["en", "sk"],
     defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+  redirects: {
+    "/": "/en/",
   },
   vite: { build: { cssMinify: 'esbuild' } },
 });
